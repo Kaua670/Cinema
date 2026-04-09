14d057c1489972ba02de59cb35346be0cfc9ee87
Nilton <moreiranilton70@gmail.com>
2026-04-08 21:58:08 -0300

    Criado Ingresso

--- src/view/Ingresso.java
+++ src/view/Ingresso.java
@@ -5,18 +5,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JTable;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ingresso extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
+	private DefaultTableModel modeloTabela;
 
 	/**
 	 * Launch the application.
@@ -51,12 +58,57 @@ public class Ingresso extends JFrame {
 		lblNewLabel.setIcon(new ImageIcon("images/image-removebg-preview.png"));
 		contentPane.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Click ");
+		table = new JTable();
+		
+		modeloTabela = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        modeloTabela.addColumn("ID"); 
+        modeloTabela.addColumn("Nome");
+        modeloTabela.addColumn("Classificação");
+
+        table = new JTable(modeloTabela);
+        
+     // esconder ID
+        table.getColumnModel().getColumn(0).setMinWidth(0);
+        table.getColumnModel().getColumn(0).setMaxWidth(0);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(32, 23, 368, 174);
+		contentPane.add(scrollPane);
+		
+		
+		scrollPane.setViewportView(table);
+		
+		
+		JButton btnNewButton = new JButton("Click");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				 int linha = table.getSelectedRow();
+				 
+				 if (linha == -1) {
+			            JOptionPane.showMessageDialog(btnNewButton, "Selecione uma filme!");
+			            return;
+			     }
+				 
+				 // Sessão
+				 
+				 
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(128, 128, 0));
 		btnNewButton.setBounds(59, 227, 89, 23);
 		contentPane.add(btnNewButton);
+		
+
+		
+
 
 	}
 }
